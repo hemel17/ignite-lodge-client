bc5f4fe2e20c3cf8ba2f748bfc6b8847c7e82188
hemel17 <redewanmahmud@gmail.com>
2024-07-14 17:47:41 +0600

    theme context added

--- src/providers/MyThemeProvider/MyThemeProvider.jsx
+++ src/providers/MyThemeProvider/MyThemeProvider.jsx
@@ -1,9 +1,27 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 const MyThemeProvider = ({ children }) => {
-  return <ThemeContext.Provider value={""}>{children}</ThemeContext.Provider>;
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export default MyThemeProvider;
