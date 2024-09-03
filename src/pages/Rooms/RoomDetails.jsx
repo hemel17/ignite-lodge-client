18040039676863285732bbc7c235977936d4e989
hemel17 <redewanmahmud@gmail.com>
2024-09-03 23:52:51 +0600

    booking started

--- src/pages/Rooms/RoomDetails.jsx
+++ src/pages/Rooms/RoomDetails.jsx
@@ -4,12 +4,17 @@ import moment from "moment";
 import { useState } from "react";
 import { Button } from "@material-tailwind/react";
 import useAuth from "../../hooks/useAuth";
+import { Bounce, toast } from "react-toastify";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import axios from "axios";
 
 const RoomDetails = () => {
   const { user } = useAuth();
-  console.log(user);
+  const email = user?.email;
   const { data } = useLoaderData();
   const {
+    _id,
     RoomDescription,
     PricePerNight,
     RoomSize,
@@ -21,23 +26,69 @@ const RoomDetails = () => {
 
   const [selectedDate, setSelectedDate] = useState(null);
 
-  const currentDate = moment().format("DDMMYY");
+  const bookingInfo = {
+    email,
+    selectedDate,
+  };
+
+  const currentDate = moment().format("YYMMDD");
 
   const handleDateChange = (e) => {
-    setSelectedDate(moment(e.target.value).format("DDMMYY"));
+    setSelectedDate(moment(e.target.value).format("YYMMDD"));
   };
 
   const handleBooking = () => {
-    if (selectedDate < currentDate) {
-      return alert("We dont't allow time travel!");
+    if (!selectedDate) {
+      return toast.error("Please select a date!", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
 
-    if (selectedDate) {
-      alert(`Booking confirmed for ${selectedDate}!`);
-      // Proceed with booking logic here
-    } else {
-      alert("Please select a date to book the room.");
+    if (selectedDate < currentDate) {
+      return toast.error("We don't allow time travel!", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
+
+    Swal.fire({
+      title: "You've selected",
+      html: `
+      <p>${RoomDescription}</p>
+      <p>$${PricePerNight} per night</p>
+      <p>${RoomSize} of heaven</p>
+      `,
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Book Now!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.put(`http://localhost:5000/rooms/${_id}`, bookingInfo);
+
+        Swal.fire({
+          title: "Welcome!",
+          text: "You've booked the room!",
+          icon: "success",
+        });
+      }
+    });
   };
 
   return (
@@ -75,8 +126,8 @@ const RoomDetails = () => {
                 Book Now
               </Button>
             ) : (
-              <button className="cursor-not-allowed pointer-events-none">
-                Book Now
+              <button className="px-4 py-2 text-red-400 border-2 border-red-400 rounded-lg cursor-not-allowed pointer-events-none">
+                Not Available!
               </button>
             )}
           </div>
